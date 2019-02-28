7d7611844dda7f5dca1b8796adaf07dd13bcdbd3
anna_akimova <anna.akimova@sitecore.com>
2019-02-28 11:17:59 +0200

    Init commit

---
+++ src/Sitecore.Support.313752/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.313752")]
+[assembly: AssemblyProduct("Sitecore.Support.313752")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.313752/QueryLatestVisitorsFromSearch.cs
@@ -0,0 +1,89 @@
+﻿namespace Sitecore.Cintel.Reporting.Aggregate.Visitors.Processors
+{
+  using System;
+  using System.Data;
+  using System.Linq;
+  using Sitecore.Analytics.Model;
+  using Sitecore.Cintel.Configuration;
+  using Sitecore.Cintel.Reporting.Processors;
+  using Sitecore.Cintel.Reporting.Utility;
+  using Sitecore.Common;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Analytics.Models;
+  using Sitecore.ContentSearch.Linq.Extensions;
+  using Sitecore.ContentSearch.Linq.Nodes;
+  using Sitecore.ContentSearch.Utilities;
+  public class QueryLatestVisitorsFromSearch : ReportProcessorBase
+  {
+    public override void Process(ReportProcessorArgs args)
+    {
+      var analyticsIndex = ContentSearchManager.GetIndex(CustomerIntelligenceConfig.ContactSearch.SearchIndexName);
+
+      using (var ctx = analyticsIndex.CreateSearchContext())
+      {
+        var rawSearchResults = ctx.GetQueryable<IndexedContact>();
+
+        args.ResultSet.TotalResultCount = ctx.GetQueryable<IndexedContact>().Count();
+
+        rawSearchResults.ForEach(sr =>
+          {
+            var row = args.ResultTableForView.NewRow();
+            BuildBaseResult(row, sr);
+            args.ResultTableForView.Rows.Add(row);
+
+            var latestVisit = ctx.GetQueryable<IndexedVisit>()
+                            .Where(iv => iv.ContactId == sr.ContactId)
+                            .OrderByDescending(iv => iv.StartDateTime)
+                                    .Take(1)
+                                    .FirstOrDefault();
+
+            if (null != latestVisit)
+            {
+              PopulateLatestVisit(latestVisit, ref row);
+            }
+          });
+      }
+    }
+
+    #region private methods
+
+    private void BuildBaseResult(DataRow row, IndexedContact ic)
+    {
+      ContactIdentificationLevel ident;
+      if (!Enum.TryParse(ic.IdentificationLevel, true, out ident))
+      {
+        ident = ContactIdentificationLevel.None;
+      }
+
+      row[Schema.ContactIdentificationLevel.Name] = (int)ident;
+      row[Schema.ContactId.Name] = ic.ContactId;
+      row[Schema.FirstName.Name] = ic.FirstName;
+      row[Schema.MiddleName.Name] = ic.MiddleName;
+      row[Schema.Surname.Name] = ic.Surname;
+      row[Schema.EmailAddress.Name] = ic.PreferredEmail;  //TODO: this needs logic
+      row[Schema.Value.Name] = ic.Value;
+      row[Schema.VisitCount.Name] = ic.VisitCount;
+      row[Schema.ValuePerVisit.Name] = Calculator.GetAverageValue(ic.Value, ic.VisitCount);
+    }
+
+    private void PopulateLatestVisit(IndexedVisit visit, ref DataRow row)
+    {
+      row[Schema.LatestVisitValue.Name] = visit.Value;
+      row[Schema.LatestVisitStartDateTime.Name] = visit.StartDateTime;
+      row[Schema.LatestVisitEndDateTime.Name] = visit.EndDateTime;
+      row[Schema.LatestVisitDuration.Name] = Calculator.GetDuration(visit.StartDateTime, visit.EndDateTime);
+
+      if (null != visit.WhoIs)
+      {
+        row[Schema.LatestVisitCityDisplayName.Name] = visit.WhoIs.City;
+        row[Schema.LatestVisitCountryDisplayName.Name] = visit.WhoIs.Country;
+        row[Schema.LatestVisitRegionDisplayName.Name] = visit.WhoIs.Region;
+      }
+    }
+
+    #endregion
+
+  }
+
+
+}
